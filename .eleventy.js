3fcd40a615874a1cccf8f22067334a9e4929cf11
Justin Aronstein <jaronstein@gmail.com>
2026-03-12 13:41:53 -0500

    Add cache headers, lazy-load below-fold images, fetchpriority on hero

--- .eleventy.js
+++ .eleventy.js
@@ -18,6 +18,7 @@ module.exports = function (eleventyConfig) {
   );
 
   eleventyConfig.addPassthroughCopy("src/_redirects");
+  eleventyConfig.addPassthroughCopy("src/_headers");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
   eleventyConfig.addPassthroughCopy("src/llms.txt");
   eleventyConfig.addPassthroughCopy("src/css");
